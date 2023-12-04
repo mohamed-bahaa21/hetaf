b586d01fe1d9f78e18434533a74dfe4980acf738
mohamed-bahaa21 <mohamedelzohary2001@outlook.com>
2023-12-04 21:01:39 +0200

    fixed: logic

---
+++ hetafs.js
@@ -0,0 +1,68 @@
+let hetafs = [
+    {
+        text: "Text 1",
+        time: 4,
+        startTime: 0,
+        endTime: 4
+    },
+    {
+        text: "Text 2",
+        time: 4,
+        startTime: 4,
+        endTime: 8
+    },
+    {
+      text: "Text 3",
+      time: 4,
+      startTime: 8,
+      endTime: 12
+    },
+    {
+      text: "Text 4",
+      time: 6,
+      startTime: 12,
+      endTime: 18
+    },
+    {
+      text: "Text 5",
+      time: 5,
+      startTime: 18,
+      endTime: 23
+    },
+    {
+      text: "Text 6",
+      time: 12,
+      startTime: 23,
+      endTime: 35
+    },
+    {
+      text: "Text 7",
+      time: 6,
+      startTime: 35,
+      endTime: 41
+    },
+    {
+      text: "Text 8",
+      time: 6,
+      startTime: 41,
+      endTime: 47
+    },
+    {
+      text: "Text 9",
+      time: 6,
+      startTime: 47,
+      endTime: 53
+    },
+    {
+      text: "Text 10",
+      time: 15,
+      startTime: 53,
+      endTime: 68
+    },
+    {
+      text: "Text 10",
+      time: 15,
+      startTime: 68,
+      endTime: 83,
+    }
+]
